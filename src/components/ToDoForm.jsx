67284dda8e33dd0bf4a310f645a2536151d05445
Diaaana03 <88292313+Diaaana03@users.noreply.github.com>
2024-06-08 01:10:44 +0600

    code

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
+import * as React from "react";
+import { pink } from "@mui/material/colors";
+import Radio from "@mui/material/Radio";
 import Task from "./Task";
 
 export default function Main() {
@@ -7,10 +10,28 @@ export default function Main() {
     JSON.parse(localStorage.getItem("tasks")) ?? []
   );
 
-  const inputRef = useRef();
-  const tasksRef = useRef();
+  // radio buttons
+
+  function ColorRadioButtons() {
+    const [selectedValue, setSelectedValue] = React.useState("a");
+
+    const handleChange = (event) => {
+      setSelectedValue(event.target.value);
+    };
+
+    const controlProps = (item) => ({
+      checked: selectedValue === item,
+      onChange: handleChange,
+      value: item,
+      name: "color-radio-button-demo",
+      inputProps: { "aria-label": item },
+    });
+  }
 
   let category = "business";
+
+  const inputRef = useRef();
+  const tasksRef = useRef();
 
   function addTask(event) {
     event.preventDefault();
@@ -92,63 +113,3 @@ export default function Main() {
     </>
   );
 }
-
-// import React, { useState } from "react";
-
-// export default function ToDoForm({ addTodo }) {
-//   let [value, setValue] = useState("");
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     addTodo(value);
-//     setValue = "";
-//   };
-//   console.log(value);
-//   return (
-//     <main>
-//       <h1 className="header__text">What's up,</h1>
-//       <input
-//         className="title"
-//         type="text"
-//         placeholder="Name here"
-//         name="title"
-//         id="title"
-//       />
-//       <h2>CREATE A TODO</h2>
-//       <form className="todo-form" onSubmit={handleSubmit}>
-//         <label htmlFor="task">What's on your todo list?</label>
-//         <input
-//           type="text"
-//           id="task"
-//           value={value}
-//           placeholder="e.g. get a milk"
-//           onChange={(e) => setValue(e.target.value)}
-//         />
-//         <label className="category__label">Pick a category</label>
-//         <div className="category">
-//           <div className="categories">
-//             <input
-//               className="category__radio"
-//               type="radio"
-//               id="business"
-//               name="category"
-//               value="business"
-//             />
-//             <label htmlFor="business">Business</label>
-//           </div>
-//           <div className="categories">
-//             <input
-//               className="category__radio personal-radio"
-//               type="radio"
-//               id="personal"
-//               name="category"
-//               value="personal"
-//             />
-//             <label htmlFor="personal">Personal</label>
-//           </div>
-//         </div>
-//         <input type="submit" id="submit__btn" value="Add Todo" />
-//         <h2>TODO List:</h2>
-//       </form>
-//     </main>
-//   );
-// }
